b8dccd43e1fb713d75a7404601bee9a497396107
Daniil Schmelzer <daniilschmelzer@gmail.com>
2022-06-15 18:02:29 +0300

    reformated code

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int price = 12333;
         int CountMile = 20;
-        int miles = price/CountMile;
-            System.out.println("Начисленные милли " + miles);
+        int miles = price / CountMile;
+        System.out.println("Начисленные милли " + miles);
     }
 }
